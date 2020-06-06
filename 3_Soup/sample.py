bb486dd0302eef81c1545ea84c532544fc0ed6a6
ma <ma_yamaki@yahoo.com>
2020-06-06 18:23:21 +0900

    mmadsfasdfad

--- 3_Soup/sample.py
+++ 3_Soup/sample.py
@@ -11,3 +11,21 @@ html_soup = BeautifulSoup(html_contents, 'html.parser')
 
 first_h1 = html_soup.find('h1')
 print(first_h1.name)
+
+print(first_h1.contents)
+
+print(str(first_h1))
+print(first_h1.text)
+print(first_h1.get_text())
+
+print(first_h1.attrs)
+print(first_h1.attrs['id'])
+print(first_h1['id'])
+
+print('----- CITATIONS -------')
+cites = html_soup.find_all('cite', class_='citation', limit=5)
+for citation in cites:
+    print(citation.get_text())
+    link = citation.find('a')
+    print(link.get('href'))
+    print()
