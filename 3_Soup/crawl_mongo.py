6edc2f826e1c2ad1590fdb550b745221078ab187
ma <ma_yamaki@yahoo.com>
2020-06-20 13:45:39 +0900

    added

---
+++ 3_Soup/crawl_mongo.py
@@ -0,0 +1,57 @@
+import re
+from typing import Iterator
+import requests
+import lxml.html
+from pymongo import MongoClient
+
+
+def main():
+    """クローラーのメイン処理"""
+    client = MongoClient('localhost',27017)
+    collection = client.scraping.ebooks
+    collection.create_index('key', unique=True)
+
+    session = requests.Session()
+    response = requests.get('https://gihyo.jp/dp')
+    urls = scrape_list_page(response)
+    for url in urls:
+        key =
+        response = session.get(url)
+        ebook = scrape_detail_page(response)
+        print(ebook)
+
+def scrape_list_page(response: requests.Response) -> Iterator[str]:
+    """
+    一覧ページのResponseから詳細ページのURLを抜き出すジェネレーター関数
+    """
+    html = lxml.html.fromstring(response.text)
+    html.make_links_absolute(response.url)
+
+    for a in html.cssselect('#listBook>li>a[itemprop="url"]'):
+        url = a.get('href')
+        yield url
+
+
+def scrape_detail_page(response: requests.Response) -> dict:
+    """
+    詳細ページのResponseから電子書籍の情報をdictで取得する
+    """
+    html = lxml.html.fromstring(response.text)
+    ebook = {
+        'url': response.url,
+        'title': html.cssselect('#bookTitle')[0].text_content(),
+        'price': html.cssselect('.buy')[0].text.strip(),
+        'content': [normalize_spaces(h3.text_content()) for h3 in html.cssselect('#content > h3')],
+    }
+    return ebook
+
+def normalize_spaces(s: str) -> str:
+    """
+    連続する空白を1つのスペースに置き換え、前後の空白を削除した新しい文字列を取得する
+    """
+    return re.sub(r'\s+', ' ', s).strip()
+
+
+
+if __name__ == '__main__':
+    main()
