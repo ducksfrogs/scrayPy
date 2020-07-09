96720df1fa8e2e8ac7c042000ee030ba1c414676
ma <ma_yamaki@yahoo.com>
2020-07-09 11:28:41 +0900

    dfadfas

--- impress/5th/selenium_first.py
+++ impress/5th/selenium_first.py
@@ -1,10 +1,14 @@
+#import chromedriver
 from selenium import webdriver
 
 url = 'http://www.webscrapingfordatascience.com/complexjavascript/'
 
-driver_path = '/usr/bin/'
-driver = webdriver.Firefox(driver_path)
+driver_path = '/usr/bin/google-chrome'
+driver = webdriver.Chrome()
 
 driver.get(url)
-input('Press ENTER to close the automated browser')
+for quote in driver.find_elements_by_class_name('quote'):
+    print(quote.text)
+
+input("Press ENTER to close the automated browser")
 driver.quit()
